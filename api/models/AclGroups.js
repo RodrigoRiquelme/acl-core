8d102b4503b66f3f4b15a83d508ed9477e3c4f2a
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2018-04-02 18:00:38 -0300

    first commit

---
+++ api/models/AclGroups.js
@@ -0,0 +1,24 @@
+/**
+ * AclGroups.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string'
+    },
+    description: {
+      type: 'string'
+    },
+    approved: {
+      type: 'string',
+      in: ['0', '1']
+    }
+  },
+
+};
+
